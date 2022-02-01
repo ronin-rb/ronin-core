83abe162d02f76831959f5da2b55ab77428b6d39
Postmodern <postmodern.mod3@gmail.com>
2022-02-01 14:33:15 -0800

    Added `Ronin::Core::Metadata::Summary` (closes #21).

---
+++ lib/ronin/core/metadata/summary.rb
@@ -0,0 +1,71 @@
+#
+# Copyright (c) 2021 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-core.
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Core
+    module Metadata
+      #
+      # Adds a {Summary::ClassMethods#summary summary} metadata
+      # attribute to a class.
+      #
+      # ### Example
+      #
+      #     class MyModule
+      #     
+      #       include Ronin::Core::Metadata::Summary
+      #     
+      #       summary "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      #     
+      #     end
+      #
+      module Summary
+        def self.included(base)
+          base.extend ClassMethods
+        end
+
+        module ClassMethods
+          #
+          # Gets or sets the summary.
+          #
+          # @param [String, nil] new_summary
+          #   The optional new summary text to set.
+          #
+          # @return [String, nil]
+          #   The previously set summary text.
+          #
+          # @example Setting the summary:
+          #   summary "Lorem ipsum dolor sit amet, consectetur adipiscing elit" 
+          #
+          # @example Getting the summary:
+          #   MyModule.summary
+          #
+          def summary(new_summary=nil)
+            if new_summary
+              @summary = new_summary
+            else
+              @summary || if superclass.kind_of?(ClassMethods)
+                                superclass.summary
+                              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/metadata/summary_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+require 'ronin/core/metadata/summary'
+
+describe Ronin::Core::Metadata::Summary do
+  describe ".summary" do
+    subject { test_class }
+
+    context "and when summary is not set in the class" do
+      module TestMetadataSummary
+        class WithNoSummarySet
+          include Ronin::Core::Metadata::Summary
+        end
+      end
+
+      let(:test_class) { TestMetadataSummary::WithNoSummarySet }
+
+      it "must default to nil" do
+        expect(subject.summary).to be(nil)
+      end
+    end
+
+    context "and when summary is set in the class" do
+      module TestMetadataSummary
+        class WithSummarySet
+          include Ronin::Core::Metadata::Summary
+
+          summary 'test'
+        end
+      end
+
+      let(:test_class) { TestMetadataSummary::WithSummarySet }
+
+      it "must return the set summary" do
+        expect(subject.summary).to eq("test")
+      end
+    end
+
+    context "but when the summary was set in the superclass" do
+      module TestMetadataSummary
+        class InheritsItsSummary < WithSummarySet
+          include Ronin::Core::Metadata::Summary
+        end
+      end
+
+      let(:test_class) { TestMetadataSummary::InheritsItsSummary }
+
+      it "must return the summary set in the superclass" do
+        expect(subject.summary).to eq("test")
+      end
+
+      context "but the summary is overridden in the sub-class" do
+        module TestMetadataSummary
+          class OverridesItsInheritedSummary < WithSummarySet
+            include Ronin::Core::Metadata::Summary
+
+            summary "test2"
+          end
+        end
+
+        let(:test_class) do
+          TestMetadataSummary::OverridesItsInheritedSummary
+        end
+
+        it "must return the summary set in the sub-class" do
+          expect(subject.summary).to eq("test2")
+        end
+      end
+    end
+  end
+end
