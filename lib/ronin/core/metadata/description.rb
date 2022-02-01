33b85032a5b73966692d0591760b37e3a4714d2d
Postmodern <postmodern.mod3@gmail.com>
2022-02-01 09:36:12 -0800

    Added `Ronin::Core::Metadata::Description` (closes #20).

---
+++ lib/ronin/core/metadata/description.rb
@@ -0,0 +1,93 @@
+#
+# Copyright (c) 2021 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-core.
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Core
+    module Metadata
+      #
+      # Adds a {Description::ClassMethods#description description} metadata
+      # attribute to a class.
+      #
+      # ### Example
+      #
+      #     class MyModule
+      #     
+      #       include Ronin::Core::Metadata::Description
+      #     
+      #       description <<~DESC
+      #         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      #         eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      #
+      #         Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      #         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+      #         dolor in reprehenderit in voluptate velit esse cillum dolore eu
+      #
+      #         fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      #         proident, sunt in culpa qui officia deserunt mollit anim id est
+      #         laborum.
+      #       DESC
+      #     
+      #     end
+      #
+      module Description
+        def self.included(base)
+          base.extend ClassMethods
+        end
+
+        module ClassMethods
+          #
+          # Gets or sets the description.
+          #
+          # @param [String, nil] new_description
+          #   The optional new description text to set.
+          #
+          # @return [String, nil]
+          #   The previously set description text.
+          #
+          # @example Setting the description:
+          #   description <<~DESC
+          #     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          #     eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          #
+          #     Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          #     laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          #     dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          #
+          #     fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          #     proident, sunt in culpa qui officia deserunt mollit anim id est
+          #     laborum.
+          #   DESC
+          #
+          # @example Getting the description:
+          #   MyModule.description
+          #
+          def description(new_description=nil)
+            if new_description
+              @description = new_description
+            else
+              @description || if superclass.kind_of?(ClassMethods)
+                                superclass.description
+                              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/metadata/description_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+require 'ronin/core/metadata/description'
+
+describe Ronin::Core::Metadata::Description do
+  describe ".description" do
+    subject { test_class }
+
+    context "and when description is not set in the shell class" do
+      module TestMetadataDescription
+        class WithNoDescriptionSet
+          include Ronin::Core::Metadata::Description
+        end
+      end
+
+      let(:test_class) { TestMetadataDescription::WithNoDescriptionSet }
+
+      it "must default to nil" do
+        expect(subject.description).to be(nil)
+      end
+    end
+
+    context "and when description is set in the shell class" do
+      module TestMetadataDescription
+        class WithDescriptionSet
+          include Ronin::Core::Metadata::Description
+
+          description 'test'
+        end
+      end
+
+      let(:test_class) { TestMetadataDescription::WithDescriptionSet }
+
+      it "must return the set description" do
+        expect(subject.description).to eq("test")
+      end
+    end
+
+    context "but when the description was set in the superclass" do
+      module TestMetadataDescription
+        class InheritsItsDescription < WithDescriptionSet
+          include Ronin::Core::Metadata::Description
+        end
+      end
+
+      let(:test_class) { TestMetadataDescription::InheritsItsDescription }
+
+      it "must return the description set in the superclass" do
+        expect(subject.description).to eq("test")
+      end
+
+      context "but the description is overridden in the sub-class" do
+        module TestMetadataDescription
+          class OverridesItsInheritedDescription < WithDescriptionSet
+            include Ronin::Core::Metadata::Description
+
+            description "test2"
+          end
+        end
+
+        let(:test_class) do
+          TestMetadataDescription::OverridesItsInheritedDescription
+        end
+
+        it "must return the description set in the superclass" do
+          expect(subject.description).to eq("test2")
+        end
+      end
+    end
+  end
+end
