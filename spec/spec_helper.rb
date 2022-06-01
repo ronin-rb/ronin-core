7d3a2ef9f69bf84dc0ad20161838f7d3a2d634d6
Postmodern <postmodern.mod3@gmail.com>
2022-06-01 15:25:08 -0700

    Remove any XDG env variables used by `Ronin::Core::Home` constants.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'rspec'
 require 'simplecov'
 SimpleCov.start
 
+ENV.delete('XDG_CACHE_HOME')
+ENV.delete('XDG_CONFIG_HOME')
+ENV.delete('XDG_DATA_HOME')
+
 require 'ronin/core/version'
 
 include Ronin
