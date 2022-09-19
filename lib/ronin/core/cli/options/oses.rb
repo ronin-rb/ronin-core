69d3aa975f232779f36087ed2d18f572faf48216
Postmodern <postmodern.mod3@gmail.com>
2022-09-18 23:57:57 -0700

    Added `Ronin::Core::CLI::Options::OSES`.

---
+++ lib/ronin/core/cli/options/oses.rb
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2021-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Core
+    module CLI
+      module Options
+        # Supported Operating Systems (OS)
+        OSES = [
+          :linux, :macos, :windows, :freebsd, :openbsd, :netbsd
+        ]
+      end
+    end
+  end
+end
