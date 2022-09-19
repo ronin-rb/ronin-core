f89064c4c6126953e7cbb0061eaa03c9f3c73fed
Postmodern <postmodern.mod3@gmail.com>
2022-09-18 23:57:41 -0700

    Added `Ronin::Core::CLI::Options::ARCHES`.

---
+++ lib/ronin/core/cli/options/arches.rb
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2021-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Core
+    module CLI
+      module Options
+        # Supported architecture names and their Symbol equivalents
+        ARCHES = {
+          'x86'      => :x86,
+          'x86-64'   => :x86_64,
+          'amd64'    => :x86_64,
+          'ia64'     => :ia64,
+          'ppc'      => :ppc,
+          'ppc64'    => :ppc64,
+          'arm'      => :arm,
+          'armbe'    => :arm_be,
+          'arm64'    => :arm64,
+          'arm64be'  => :arm64_be,
+          'mips'     => :mips,
+          'mipsle'   => :mips_le,
+          'mips64'   => :mips64,
+          'mips64le' => :mips64_le
+        }
+      end
+    end
+  end
+end
---
+++ spec/cli/options/arches_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require 'ronin/core/cli/options/arches'
+
+describe "Ronin::Core::CLI::Options::ARCHES" do
+  subject { Ronin::Core::CLI::Options::ARCHES }
+
+  it "must map 'x86' to :x86" do
+    expect(subject['x86']).to eq(:x86)
+  end
+
+  it "must map 'x86-64' to :x86_64" do
+    expect(subject['x86-64']).to eq(:x86_64)
+  end
+
+  it "must map 'amd64' to :x86_64" do
+    expect(subject['amd64']).to eq(:x86_64)
+  end
+
+  it "must map 'ia64' to :ia64" do
+    expect(subject['ia64']).to eq(:ia64)
+  end
+
+  it "must map 'ppc' to :ppc" do
+    expect(subject['ppc']).to eq(:ppc)
+  end
+
+  it "must map 'ppc64' to :ppc64" do
+    expect(subject['ppc64']).to eq(:ppc64)
+  end
+
+  it "must map 'arm' to :arm" do
+    expect(subject['arm']).to eq(:arm)
+  end
+
+  it "must map 'armbe' to :arm_be" do
+    expect(subject['armbe']).to eq(:arm_be)
+  end
+
+  it "must map 'arm64' to :arm64" do
+    expect(subject['arm64']).to eq(:arm64)
+  end
+
+  it "must map 'arm64be' to :arm64_be" do
+    expect(subject['arm64be']).to eq(:arm64_be)
+  end
+
+  it "must map 'mips' to :mips" do
+    expect(subject['mips']).to eq(:mips)
+  end
+
+  it "must map 'mipsle' to :mips_le" do
+    expect(subject['mipsle']).to eq(:mips_le)
+  end
+
+  it "must map 'mips64' to :mips64" do
+    expect(subject['mips64']).to eq(:mips64)
+  end
+
+  it "must map 'mips64le' to :mips64_le" do
+    expect(subject['mips64le']).to eq(:mips64_le)
+  end
+end
