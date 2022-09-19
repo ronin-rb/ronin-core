d0a95fbfa98943eab86a9a080b59d2d0586ec7c1
Postmodern <postmodern.mod3@gmail.com>
2022-09-19 15:50:15 -0700

    Added `CLI::Generator::Options::Author`.
    * Defines `-a,--author` and `-e,--author-email` options.

---
+++ lib/ronin/core/cli/generator/options/author.rb
@@ -0,0 +1,105 @@
+#
+# Copyright (c) 2021-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/core/git'
+
+module Ronin
+  module Core
+    module CLI
+      module Generator
+        module Options
+          #
+          # Adds the `-a,--author NAME` and `-e,--author-email EMAIL` options
+          # for the generator command.
+          #
+          module Author
+
+            #
+            # The default author name.
+            #
+            # @return [String, nil]
+            #
+            def self.default_name
+              Core::Git.user_name || ENV['USERNAME']
+            end
+
+            #
+            # The default author email.
+            #
+            # @return [String, nil]
+            #
+            def self.default_email
+              Core::Git.user_email
+            end
+
+            #
+            # Defines the `-a,--author NAME` and `-e,--author-email EMAIL`
+            # options.
+            #
+            # @param [Class<Comand>] command
+            #   The command class including {Author}.
+            #
+            def self.included(command)
+              command.option :author, short: '-a',
+                                      value: {
+                                        type:    String,
+                                        usage:   'NAME',
+                                        default: ->{ default_name }
+                                      },
+                                      desc: 'The name of the author' do |author|
+                                        @author_name = author
+                                      end
+
+              command.option :author_email, short: '-e',
+                                            value: {
+                                              type:    String,
+                                              usage:   'EMAIL',
+                                              default: ->{ default_email }
+                                            },
+                                            desc: 'The email address of the author' do |email|
+                                              @author_email = email
+                                            end
+            end
+
+            # The author name.
+            #
+            # @return [String]
+            attr_reader :author_name
+
+            # The author email.
+            #
+            # @return [String]
+            attr_reader :author_email
+
+            #
+            # Initializes {#author} and {#author_email}.
+            #
+            # @param [Hash{Symbol => Object}] kwargs
+            #   Additional keyword arguments.
+            #
+            def initialize(**kwargs)
+              super(**kwargs)
+
+              @author_name  = Author.default_name
+              @author_email = Author.default_email
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/cli/generator/options/author_spec.rb
@@ -0,0 +1,109 @@
+require 'spec_helper'
+require 'ronin/core/cli/generator/options/author'
+require 'ronin/core/cli/command'
+require 'ronin/core/cli/generator'
+
+describe Ronin::Core::CLI::Generator::Options::Author do
+  module TestAuthorOption
+    class TestCommand < Ronin::Core::CLI::Command
+      include Ronin::Core::CLI::Generator
+      include Ronin::Core::CLI::Generator::Options::Author
+
+      template_dir 'test'
+    end
+  end
+
+  let(:command_class) { TestAuthorOption::TestCommand }
+  subject { command_class.new }
+
+  let(:name)  { 'Foo Bar' }
+  let(:email) { 'foo.bar@example.com' }
+
+  describe ".defaultor_name" do
+    subject { described_class }
+
+    context "when `git config user.name` is set" do
+      before { allow(Ronin::Core::Git).to receive(:user_name).and_return(name) }
+
+      it "must use `git config user.name`" do
+        expect(subject.default_name).to eq(name)
+      end
+    end
+    
+    context "when `git config user.name` isn't set" do
+      before { allow(Ronin::Core::Git).to receive(:user_name).and_return(nil) }
+
+      it "must fallback to using the USERNAME environment variable" do
+        expect(subject.default_name).to eq(ENV['USERNAME'])
+      end
+    end
+  end
+
+  describe ".default_email" do
+    subject { described_class }
+
+    before { allow(Ronin::Core::Git).to receive(:user_email).and_return(email) }
+
+    it "must use `git config user.email`" do
+      expect(subject.default_email).to eq(email)
+    end
+  end
+
+  describe ".included" do
+    subject { command_class }
+
+    it "must add a '-a,--author NAME' option" do
+      expect(subject.options[:author]).to_not be_nil
+      expect(subject.options[:author].short).to eq('-a')
+      expect(subject.options[:author].value).to_not be_nil
+      expect(subject.options[:author].value.default.call).to eq(described_class.default_name)
+      expect(subject.options[:author].value.type).to eq(String)
+      expect(subject.options[:author].value.usage).to eq('NAME')
+      expect(subject.options[:author].desc).to eq("The name of the author (Default: #{described_class.default_name})")
+      expect(subject.options[:author].block).to_not be_nil
+    end
+
+    it "must add a '-e,--author-email EMAIL' option" do
+      expect(subject.options[:author_email]).to_not be_nil
+      expect(subject.options[:author_email].short).to eq('-e')
+      expect(subject.options[:author_email].value).to_not be_nil
+      expect(subject.options[:author_email].value.default.call).to eq(described_class.default_email)
+      expect(subject.options[:author_email].value.type).to eq(String)
+      expect(subject.options[:author_email].value.usage).to eq('EMAIL')
+      expect(subject.options[:author_email].desc).to eq("The email address of the author (Default: #{described_class.default_email})")
+      expect(subject.options[:author_email].block).to_not be_nil
+    end
+  end
+
+  describe "#initialize" do
+    it "must set #author_name to #{described_class}.default_name" do
+      expect(subject.author_name).to eq(described_class.default_name)
+    end
+
+    it "must set #author_email to #{described_class}.default_email" do
+      expect(subject.author_email).to eq(described_class.default_email)
+    end
+  end
+
+  describe "#parse_options" do
+    context "when given '--author NAME'" do
+      let(:argv) { ['--author', name] }
+
+      before { subject.parse_options(argv) }
+
+      it "must set #uathor_name" do
+        expect(subject.author_name).to eq(name)
+      end
+    end
+
+    context "when given '--author-email NAME'" do
+      let(:argv) { ['--author-email', email] }
+
+      before { subject.parse_options(argv) }
+
+      it "must set #uathor_email" do
+        expect(subject.author_email).to eq(email)
+      end
+    end
+  end
+end
