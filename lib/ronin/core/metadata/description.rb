42250f772b2e2878c65564dd237c15edbe0e325a
Postmodern <postmodern.mod3@gmail.com>
2023-02-23 13:38:00 -0800

    Added a top-level description for `Ronin::Core::Metadata::Description::ClassMethods`.

--- lib/ronin/core/metadata/description.rb
+++ lib/ronin/core/metadata/description.rb
@@ -57,6 +57,9 @@ module Ronin
           base.extend ClassMethods
         end
 
+        #
+        # Class-methods.
+        #
         module ClassMethods
           #
           # Gets or sets the description.
