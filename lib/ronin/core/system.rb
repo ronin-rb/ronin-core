09deeb7607e7aeeceaf2cf68e09192f08db537dd
Postmodern <postmodern.mod3@gmail.com>
2023-07-11 18:30:33 -0700

    Added `Ronin::Core::System` (closes #38).
    * Supports detectingthe OS type.
    * Supports parsing the `PATH` env variable.
    * Supports checking if a command is installed.
    * Supports downloading files using `wget` or `curl`.

---
+++ lib/ronin/core/system.rb
@@ -0,0 +1,144 @@
+# frozen_string_literal: true
+#
+# Copyright (c) 2021-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-core.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'uri'
+require 'fileutils'
+
+module Ronin
+  module Core
+    #
+    # Utility methods for interacting with the system.
+    #
+    # @api semipublic
+    #
+    # @since 0.2.0
+    #
+    module System
+      #
+      # The detected Operating System type.
+      #
+      # @return [:linux, :macos, :freebsd, :openbsd, :netbsd, :windows, nil]
+      #
+      def self.os
+        @os ||= if    RUBY_PLATFORM.include?('linux')   then :linux
+                elsif RUBY_PLATFORM.include?('darwin')  then :macos
+                elsif RUBY_PLATFORM.include?('freebsd') then :freebsd
+                elsif RUBY_PLATFORM.include?('openbsd') then :openbsd
+                elsif RUBY_PLATFORM.include?('netbsd')  then :netbsd
+                elsif Gem.win_platform?                 then :windows
+                end
+      end
+
+      #
+      # The list of `bin/` directories containing executables.
+      #
+      # @return [Array<String>]
+      #
+      def self.paths
+        @paths ||= if ENV['PATH']
+                     ENV['PATH'].split(File::PATH_SEPARATOR)
+                   else
+                     []
+                   end
+      end
+
+      #
+      # Determines if a command is installed.
+      #
+      # @param [String] command
+      #   The command name to search for.
+      #
+      # @return [Boolean]
+      #   Indicates whether the command was found or not.
+      #
+      def self.installed?(command)
+        paths.any? do |bin_dir|
+          File.executable?(File.join(bin_dir,command))
+        end
+      end
+
+      #
+      # The detected download command.
+      #
+      # @return ['curl', 'wget', nil]
+      #
+      # @api private
+      #
+      def self.downloader
+        @downloader ||= if os == :macos then :curl
+                        else
+                          if    installed?('wget') then :wget
+                          elsif installed?('curl') then :curl
+                          end
+                        end
+      end
+
+      #
+      # Exception class that indicates a download failed.
+      #
+      class DownloadFailed < RuntimeError
+      end
+
+      #
+      # Downloads a file using either `curl` or `wget`.
+      #
+      # @param [URI::HTTP, String] url
+      #   The URL to download.
+      #
+      # @param [String] dest
+      #   The destination file or directory to download into.
+      #
+      # @return [String]
+      #   The path to the downloaded file.
+      #
+      # @raise [DownloadFailed]
+      #   The download failed or `curl`/`wget` could not be found on the system.
+      #
+      def self.download(url,dest)
+        uri = URI(url)
+        url = url.to_s
+
+        if File.directory?(dest)
+          dest = File.join(dest,File.basename(uri.path))
+        end
+
+        FileUtils.mkdir_p(File.dirname(dest))
+
+        partial_dest = "#{dest}.part"
+
+        case downloader
+        when :wget
+          unless system('wget','-c','-O',partial_dest,url)
+            raise(DownloadFailed,"wget command failed: wget -c -O #{partial_dest} #{url}")
+          end
+        when :curl
+          unless system('curl','-f','-L','-C','-','-o',partial_dest,url)
+            raise(DownloadFailed,"curl command failed: curl -f -L -C - -o #{partial_dest} #{url}")
+          end
+        when nil
+          raise(DownloadFailed,"could not find 'curl' or 'wget' on the system")
+        else
+          raise(NotImplementedError,"downloader not supported: #{downloader.inspect}")
+        end
+
+        FileUtils.mv(partial_dest,dest)
+        return dest
+      end
+    end
+  end
+end
---
+++ spec/system_spec.rb
@@ -0,0 +1,268 @@
+require 'spec_helper'
+require 'ronin/core/system'
+
+require 'tmpdir'
+
+describe Ronin::Core::System do
+  describe ".os" do
+    it "must memoize the result" do
+      expect(subject.os).to be(subject.os)
+    end
+
+    context "when RUBY_PLATFORM contains 'linux'" do
+      before { stub_const('RUBY_PLATFORM','x86_64-linux') }
+
+      it { expect(subject.os).to be(:linux) }
+    end
+
+    context "when RUBY_PLATFORM contains 'darwin'" do
+      before { stub_const('RUBY_PLATFORM','aarch64-darwin') }
+
+      it { expect(subject.os).to be(:macos) }
+    end
+
+    context "when RUBY_PLATFORM contains 'freebsd'" do
+      before { stub_const('RUBY_PLATFORM','x86_64-freebsd') }
+
+      it { expect(subject.os).to be(:freebsd) }
+    end
+
+    context "when RUBY_PLATFORM contains 'openbsd'" do
+      before { stub_const('RUBY_PLATFORM','x86_64-openbsd') }
+
+      it { expect(subject.os).to be(:openbsd) }
+    end
+
+    context "when RUBY_PLATFORM contains 'netbsd'" do
+      before { stub_const('RUBY_PLATFORM','x86_64-netbsd') }
+
+      it { expect(subject.os).to be(:netbsd) }
+    end
+
+    context "when Gem.win_platform? returns true" do
+      before do
+        stub_const('RUBY_PLATFORM','x86_64-mswin')
+
+        expect(Gem).to receive(:win_platform?).and_return(true)
+      end
+
+      it { expect(subject.os).to be(:windows) }
+    end
+
+    after { subject.instance_variable_set(:@os,nil) }
+  end
+
+  describe ".paths" do
+    it "must return a split Array of $PATH" do
+      expect(subject.paths).to eq(ENV['PATH'].split(File::PATH_SEPARATOR))
+    end
+
+    it "must memoize the result" do
+      expect(subject.paths).to be(subject.paths)
+    end
+
+    context "when $PATH is not set" do
+      before do
+        stub_const('ENV',{})
+      end
+
+      it "must return []" do
+        expect(subject.paths).to eq([])
+      end
+    end
+
+    after { subject.instance_variable_set(:@paths,nil) }
+  end
+
+  describe ".installed?" do
+    context "when the executable is in one of the .paths directories" do
+      it "must return true" do
+        expect(subject.installed?('ruby')).to be(true)
+      end
+    end
+
+    context "when the executable is not in any of the .paths directories" do
+      it "must return false" do
+        expect(subject.installed?('does_not_exist')).to be(false)
+      end
+    end
+  end
+
+  describe ".downloader" do
+    context "when on macOS" do
+      before { expect(subject).to receive(:os).and_return(:macos) }
+
+      it "must default to :curl" do
+        expect(subject.downloader).to be(:curl)
+      end
+    end
+
+    context "otherwise" do
+      context "when both wget and curl are installed" do
+        before do
+          expect(subject).to receive(:installed?).with('wget').and_return(true)
+          expect(subject).to_not receive(:installed?).with('curl')
+        end
+
+        it "must return :wget" do
+          expect(subject.downloader).to be(:wget)
+        end
+      end
+
+      context "when wget is installed, but curl is not" do
+        before do
+          expect(subject).to receive(:installed?).with('wget').and_return(true)
+          expect(subject).to_not receive(:installed?).with('curl')
+        end
+
+        it "must return :wget" do
+          expect(subject.downloader).to be(:wget)
+        end
+      end
+
+      context "when curl is installed, but wget is not" do
+        before do
+          expect(subject).to receive(:installed?).with('wget').and_return(false)
+          expect(subject).to receive(:installed?).with('curl').and_return(true)
+        end
+
+        it "must return :curl" do
+          expect(subject.downloader).to be(:curl)
+        end
+      end
+    end
+
+    after { subject.instance_variable_set(:@downloader,nil) }
+  end
+
+  describe ".download" do
+    let(:url)    { 'https://example.com/test.txt' }
+    let(:uri)    { URI.parse(url) }
+    let(:tmpdir) { Dir.mktmpdir }
+
+    context "when the destination is a file" do
+      let(:dest) { File.join(tmpdir,'dest.txt') }
+
+      context "when .downloader is :wget" do
+        before { expect(subject).to receive(:downloader).and_return(:wget) }
+
+        it "must ensure the parent exists, execute 'wget -c -O \#{dest}.part \#{url}', rename the file, and return true" do
+          expect(FileUtils).to receive(:mkdir_p).with(File.dirname(dest))
+          expect(subject).to receive(:system).with('wget','-c','-O',"#{dest}.part",url).and_return(true)
+          expect(FileUtils).to receive(:mv).with("#{dest}.part",dest)
+
+          expect(subject.download(uri,dest)).to eq(dest)
+        end
+
+        context "but the command fails" do
+          it "must not rename the file, but raise a Ronin::Core::System::DownloadFailed exception" do
+            expect(FileUtils).to receive(:mkdir_p).with(File.dirname(dest))
+            expect(subject).to receive(:system).with('wget','-c','-O',"#{dest}.part",url).and_return(false)
+            expect(FileUtils).to_not receive(:mv).with("#{dest}.part",dest)
+
+            expect {
+              subject.download(uri,dest)
+            }.to raise_error(Ronin::Core::System::DownloadFailed,"wget command failed: wget -c -O #{dest}.part #{url}")
+          end
+        end
+      end
+
+      context "when .downloader is :curl" do
+        before { expect(subject).to receive(:downloader).and_return(:curl) }
+
+        it "must ensure the parent exists, execute 'curl -f -L -C - -o \#{dest}.part \#{url}', rename the file, and return true" do
+          expect(FileUtils).to receive(:mkdir_p).with(File.dirname(dest))
+          expect(subject).to receive(:system).with('curl','-f','-L','-C','-','-o',"#{dest}.part",url).and_return(true)
+          expect(FileUtils).to receive(:mv).with("#{dest}.part",dest)
+
+          expect(subject.download(uri,dest)).to eq(dest)
+        end
+
+        context "but the command fails" do
+          it "must not rename the file, but raise a Ronin::Core::System::DownloadFailed exception" do
+            expect(FileUtils).to receive(:mkdir_p).with(File.dirname(dest))
+            expect(subject).to receive(:system).with('curl','-f','-L','-C','-','-o',"#{dest}.part",url).and_return(false)
+            expect(FileUtils).to_not receive(:mv).with("#{dest}.part",dest)
+
+            expect {
+              subject.download(uri,dest)
+            }.to raise_error(Ronin::Core::System::DownloadFailed,"curl command failed: curl -f -L -C - -o #{dest}.part #{url}")
+          end
+        end
+      end
+
+      context "when .downloader is nil" do
+        before { expect(subject).to receive(:downloader).and_return(nil) }
+
+        it do
+          expect {
+            subject.download(uri,dest)
+          }.to raise_error(Ronin::Core::System::DownloadFailed,"could not find 'curl' or 'wget' on the system")
+        end
+      end
+    end
+
+    context "when the destination is a directory" do
+      let(:dest) { tmpdir }
+      let(:derived_dest) { File.join(dest,File.basename(uri.path)) }
+
+      context "when .downloader is :wget" do
+        before { expect(subject).to receive(:downloader).and_return(:wget) }
+
+        it "must ensure the dest directory exists, execute 'wget -c -O \#{dest}/\#{file_name}.part \#{url}', rename the file, and return true" do
+          expect(FileUtils).to receive(:mkdir_p).with(File.dirname(derived_dest))
+          expect(subject).to receive(:system).with('wget','-c','-O',"#{derived_dest}.part",url).and_return(true)
+          expect(FileUtils).to receive(:mv).with("#{derived_dest}.part",derived_dest)
+
+          expect(subject.download(uri,dest)).to eq(derived_dest)
+        end
+
+        context "but the command fails" do
+          it "must not rename the file, but raise a Ronin::Core::System::DownloadFailed exception" do
+            expect(FileUtils).to receive(:mkdir_p).with(File.dirname(derived_dest))
+            expect(subject).to receive(:system).with('wget','-c','-O',"#{derived_dest}.part",url).and_return(false)
+            expect(FileUtils).to_not receive(:mv).with("#{derived_dest}.part",derived_dest)
+
+            expect {
+              subject.download(uri,dest)
+            }.to raise_error(Ronin::Core::System::DownloadFailed,"wget command failed: wget -c -O #{derived_dest}.part #{url}")
+          end
+        end
+      end
+
+      context "when .downloader is :curl" do
+        before { expect(subject).to receive(:downloader).and_return(:curl) }
+
+        it "must ensure the dest directory exists, execute 'curl -f -L -C - -o \#{dest}\#{file_name}.part \#{url}', rename the file, and return the path to the downloaded file" do
+          expect(FileUtils).to receive(:mkdir_p).with(File.dirname(derived_dest))
+          expect(subject).to receive(:system).with('curl','-f','-L','-C','-','-o',"#{derived_dest}.part",url).and_return(true)
+          expect(FileUtils).to receive(:mv).with("#{derived_dest}.part",derived_dest)
+
+          expect(subject.download(uri,dest)).to eq(derived_dest)
+        end
+
+        context "but the command fails" do
+          it "must not rename the file, but raise a Ronin::Core::System::DownloadFailed exception" do
+            expect(FileUtils).to receive(:mkdir_p).with(File.dirname(derived_dest))
+            expect(subject).to receive(:system).with('curl','-f','-L','-C','-','-o',"#{derived_dest}.part",url).and_return(false)
+            expect(FileUtils).to_not receive(:mv).with("#{derived_dest}.part",derived_dest)
+
+            expect {
+              subject.download(uri,dest)
+            }.to raise_error(Ronin::Core::System::DownloadFailed,"curl command failed: curl -f -L -C - -o #{derived_dest}.part #{url}")
+          end
+        end
+      end
+
+      context "when .downloader is nil" do
+        before { expect(subject).to receive(:downloader).and_return(nil) }
+
+        it do
+          expect {
+            subject.download(uri,dest)
+          }.to raise_error(Ronin::Core::System::DownloadFailed,"could not find 'curl' or 'wget' on the system")
+        end
+      end
+    end
+  end
+end
